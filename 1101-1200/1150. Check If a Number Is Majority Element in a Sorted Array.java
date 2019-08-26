e741d6462ef22129898fe35407c6b01c188ef906
jian <jianwang400@gmail.com>
2019-08-25 23:09:13 -0500

    Add: add 1150. Check If a Number Is Majority Element in a Sorted Array

---
+++ 1101-1200/1150. Check If a Number Is Majority Element in a Sorted Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isMajorityElement(int[] nums, int target) {
+        int count = 0;
+        for(int num : nums){
+            if(num == target){
+                count++;
+            }
+        }
+        return count > nums.length / 2;
+    }
+}
+/*
+Topic: array
+Time: O(N)
+Space: O(1)
+*/
