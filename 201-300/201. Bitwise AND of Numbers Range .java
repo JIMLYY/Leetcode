babff72d3f889320ac1f7bd7f9fcb7e5f5fd9440
jian <jianwang400@gmail.com>
2019-09-14 23:51:33 -0500

    Add: add 201. Bitwise AND of Numbers Range .java

---
+++ 201-300/201. Bitwise AND of Numbers Range .java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        if(m == 0) return 0;
+        int moveFactor = 0;
+        while(m != n){
+            m >>= 1;
+            n >>= 1;
+            moveFactor ++;
+        }
+        return m << moveFactor;
+    }
+}
+/*
+Topic: bit manipulation  
+*/
